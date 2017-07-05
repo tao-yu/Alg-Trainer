f8d6722deba2be8a80cb23a4442d1d3c73ca120d
Tao-Yu <taoxyu@gmail.com>
2017-07-05 17:41:18 +0100

    Removed useless 2gll array + tided up unused files

--- sim.js
+++ sim.js
@@ -1,45 +1,5 @@
 (function(){
 
-	set_zbll = {
-		"t_2gll" : [
-			"U' (R U R' U R U2 R') U2 (R' U' R U' R' U2 R)",
-			"U' (R U R' U R U2 R') U' (R U2 R' U' R U' R')",
-			"U' (R' U' R U' R' U2 R) U (R' U2 R U R' U R)",
-			"R U2 R' U' R U' R2 U2 R U R' U R",
-			"U2 R' U2 R U R' U R2 U2 R' U' R U' R'",
-			"U' R U R2 U' R2 U' R2 U2 R U' R U' R'",
-			"U' R' U' R2 U R2 U R2 U2 R' U R' U R",
-			"U2 (R' U2 R U R' U R) U' (R' U' R U' R' U2 R)",
-			"(R U2 R' U' R U' R') U (R U R' U R U2 R')",
-			"U' R' U' R U' R' U R U' R U R2 U R2 U2 R'",
-			"U' R U R' U R U' R' U R' U' R2 U' R2 U2 R",
-			"R' U R U2 R' U' R U' R U R' U' R' U' R U R U' R'"]
-		,
-		"pi_2gll" : [
-			"(R U2 R' U' R U' R') U' (R U2 R' U' R U' R')",
-			"R' U2 R2 U R2 U R2 U2 R'",
-			"[U'] R U2 R' U2 R U' R' U2 R U' R' U2 R U R'",
-			"R' U2 R U2 R' U R U2 R' U R U2 R' U' R",
-			"(R U R' U R U2 R') U' (R U R' U R U2 R')",
-			"(R' U' R U' R' U2 R) U (R' U' R U' R' U2 R)",
-			"R' U2 R U R' U R2 U R' U R U2 R'",
-			"R U2 R' U' R U' R2 U' R U' R' U2 R",
-			"R U2 R2 U' R2 U' R2 U2 R",
-			"R U' R' U2 R U R' U2 R U R' U2 R U2 R'",
-			"[U2] R' U R U2 R' U' R U2 R' U' R U2 R' U2 R",
-			"F R U R' U' R U R' U' F' R U R' U' L R' F R F' L'",
-		]
-		,
-		"h_2gll" : ["(R U2 R' U' R U' R') U' (R' U' R U' R' U2 R)",
-			"(R' U2 R U R' U R) U (R U R' U R U2 R')",
-			"R U R' U R' U' R U R' U' R2 U' R2 U R U2 R' U2 R",
-			"R U2 R' U' R U R' U' R U' R'",
-			"R' U2 R U R' U' R U R' U R",
-			"R' U' R U' R' U R U' R' U2 R",
-			"R U R' U R U' R' U R U2 R'",
-			"(R' U' R U' R' U2 R) U (R U2 R' U' R U' R')"]
-	};
-
 	solved = [1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,6];
 	var currentRotation = 0;
 	var cube=solved;
